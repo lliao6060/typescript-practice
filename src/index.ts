23673aee00aa84b0a4eb60b9fc0d934509ef9e55
MOMODA1\FG000098 <meiliao1207@gmail.com>
2022-05-04 16:06:16 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// const obj = {
+//   say(name: string) {
+//     console.log('Hello: ', name);
+//   },
+// };
+
+// function test(this: typeof obj, str: string) {
+//   console.log(this.say(str));
+// }
